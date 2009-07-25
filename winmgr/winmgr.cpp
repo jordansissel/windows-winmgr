1af71893826a02ba63a1c2cd8e505ab0704df31a
Jordan Sissel <jls@semicomplete.com>
2009-07-25 07:35:28 +0000

    git-svn-id: https://semicomplete.googlecode.com/svn/winmgr@2227 66067f73-fe4c-0410-82e9-b9c6d0c95a22

--- winmgr/winmgr.cpp
+++ winmgr/winmgr.cpp
@@ -10,14 +10,15 @@
 #using <mscorlib.dll>
 
 using namespace System::Collections;
-using namespace System::Threading;
+using namespace System::ComponentModel;
+using namespace System::Windows::Threading;
 using namespace System::IO;
 using namespace System::Runtime::InteropServices;
 using namespace System::Windows::Markup;
 using namespace System::Windows::Interop;
 using namespace System::Windows;
 using namespace System::Windows::Controls;
-using namespace System::Windows::Input;
+using namespace System::Windows::Input;       
 using namespace System;
 using namespace System::Xml;
 using namespace System::Resources;
@@ -31,6 +32,8 @@ using namespace winmgr;
 public ref class WindowManager : Application {
   public:
     WindowSearcher ^wsearch;
+    BackgroundWorker ^hotkeyworker;
+
     WindowManager() {
       this->Initialize();
     }
@@ -39,22 +42,25 @@ public ref class WindowManager : Application {
       XmlTextReader ^input = gcnew XmlTextReader(gcnew StringReader(xaml));
 
       //Stream ^input = File::OpenRead("mainwindow.xaml";)
-    Window ^window = (Window ^)XamlReader::Load(input);
+      Window ^window = (Window ^)XamlReader::Load(input);
       input->Close();
       return window;
     }
-	
-	void activate() {
+
+    void activate() {
       this->wsearch->start();
       ItemsControl ^l = (ItemsControl ^)this->MainWindow->FindName("results");
       l->ItemsSource = this->wsearch->windows;
-      ((TextBox ^)this->MainWindow->FindName("userinput"))->Focus();
-
-	}
+      TextBox ^input = ((TextBox ^)this->MainWindow->FindName("userinput"));
+	  input->Text = "";
+	  input->Focus();
+      this->MainWindow->Show();
+	  this->MainWindow->Activate();
+    }
 
     void onloaded(Object ^sender, RoutedEventArgs ^ev) {
-		/* Nothing */
-	}
+      /* Nothing */
+    }
 
     void ontextinput(Object ^sender, TextChangedEventArgs ^ev) {
       TextBox ^input = (TextBox ^)sender;
@@ -71,6 +77,7 @@ public ref class WindowManager : Application {
           ((WindowItem ^)windows[0])->activate();
         }
         ev->Handled = true;
+		this->MainWindow->Hide();
       } else if (ev->Key == Key::Escape) {
         /* Destroy window */
         //Window ^main = Application::Current->MainWindow;
@@ -85,16 +92,16 @@ public ref class WindowManager : Application {
       }
     }
 
-  void app_onstartup(Object ^sender, StartupEventArgs ^ev) {
-    this->MainWindow->Show();
-  }
+    void app_onstartup(Object ^sender, StartupEventArgs ^ev) {
+    }
+
 
     void Initialize() {
+      this->hotkeyworker = gcnew BackgroundWorker();
       this->wsearch = gcnew winmgr::WindowSearcher();
       this->MainWindow = WindowFromXaml(MAINWINDOW_XAML);
       this->MainWindow->Height = 200;
       this->MainWindow->Width = 600;
-      //main->Title = "Testing XAML";
       this->MainWindow->Loaded += gcnew RoutedEventHandler(this, &WindowManager::onloaded);
 
       TextBox ^input = (TextBox ^)this->MainWindow->FindName("userinput");
@@ -103,41 +110,41 @@ public ref class WindowManager : Application {
       input->KeyDown += gcnew KeyEventHandler(this, &WindowManager::input_onkeypress);
       this->MainWindow->KeyDown += gcnew KeyEventHandler(this, &WindowManager::window_onkeypress);
       this->Startup += gcnew StartupEventHandler(this, &WindowManager::app_onstartup);
+
+      this->hotkeyworker->DoWork += \
+        gcnew DoWorkEventHandler(this, &WindowManager::BackgroundHotKeyHandler);
+      this->hotkeyworker->RunWorkerAsync();
     }
 
-};
-
-public ref class HotKeyThread {
-private:
-  WindowManager ^wm;
-
-public:
-  HotKeyThread(WindowManager ^wm) {
-    this->wm = wm;
-  }
-
-  void ThreadProc() {
-    tagMSG msg = {0};
-    while (GetMessage(&msg, NULL, 0, 0) != 0) {
-      if (msg.message == WM_HOTKEY) {
-        wm->activate();
+    void BackgroundHotKeyHandler(Object ^sender, DoWorkEventArgs ^ev) {
+      tagMSG msg = {0};
+      BackgroundWorker ^bw = (BackgroundWorker ^)sender;
+      RegisterHotKey(NULL, 1, MOD_ALT, VK_SPACE);
+      while (GetMessage(&msg, NULL, 0, 0) != 0) {
+        if (msg.message == WM_HOTKEY) {
+          //MessageBox::Show("Hello");
+          //bw->ReportProgress(0); /* lame hack */
+	      this->Dispatcher->Invoke(DispatcherPriority::Normal,
+			                       gcnew voiddelegate(this, &WindowManager::activate));
+        }
       }
-	}
-  }
- };
+      UnregisterHotKey(NULL, 1);
+    }
+
+	delegate void voiddelegate();
+
+    void BackgroundHotKeyActivated(Object ^sender, ProgressChangedEventArgs ^ev) {
+		this->Dispatcher->Invoke(System::Windows::Threading::DispatcherPriority::Normal,
+			gcnew voiddelegate(this, &WindowManager::activate));
+    }
+};
 
 [STAThreadAttribute]
 int main(array<System::String ^> ^args) {
-  if (RegisterHotKey(NULL, 1, MOD_ALT, VK_SPACE)) {
-    /* Safe */
-  } else {
-    /* fail */
-  }
-
   WindowManager ^wm = gcnew WindowManager();
-  HotKeyThread ^hkt = gcnew HotKeyThread(wm);
-  Thread ^hotkeythread = gcnew Thread(gcnew ThreadStart(hkt, &HotKeyThread::ThreadProc));
-  hotkeythread->Start();
+  //HotKeyThread ^hkt = gcnew HotKeyThread(wm);
+  //Thread ^hotkeythread = gcnew Thread(gcnew ThreadStart(hkt, &HotKeyThread::ThreadProc));
+  //hotkeythread->Start();
 
   return wm->Run();
 }
